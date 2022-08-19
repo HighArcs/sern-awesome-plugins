55295e7147246c4da3e9ef82d6475754df585d55
Arcy <higharcswastaken@gmail.com>
2022-08-18 21:59:37 -0500

    a

--- src/intents.ts
+++ src/intents.ts
@@ -1,11 +1,15 @@
 import { CommandType, EventPlugin, PluginType } from "@sern/handler";
 import {
 	BitFieldResolvable,
+	Client,
 	GatewayIntentsString,
 	IntentsBitField,
 } from "discord.js";
 export function intents<T extends CommandType>(
-	intents: BitFieldResolvable<GatewayIntentsString, number>
+	intents: BitFieldResolvable<GatewayIntentsString, number>,
+	onIntentError: (err: IntentsError) => any = (err) => {
+		throw err;
+	}
 ): EventPlugin<T> {
 	return {
 		name: "intents",
@@ -16,10 +20,8 @@ export function intents<T extends CommandType>(
 			const missing = bf.missing(intents);
 
 			if (missing.length > 0) {
-				console.error(
-					`Event ${event[0].id} expects intents ${missing.join(
-						", "
-					)} but bot has ${bf.toArray().join(", ")}`
+				onIntentError(
+					new IntentsError(event[0].id, event[0].client, intents)
 				);
 				return controller.stop();
 			}
@@ -27,4 +29,27 @@ export function intents<T extends CommandType>(
 			return controller.next();
 		},
 	};
+}
+
+export class IntentsError extends Error {
+	public client: Client;
+	public expect: IntentsBitField;
+	public actual: IntentsBitField;
+	constructor(
+		id: string,
+		client: Client,
+		expect: BitFieldResolvable<GatewayIntentsString, number>
+	) {
+		const bf = new IntentsBitField(client.options.intents);
+		const x = new IntentsBitField(expect);
+		super(
+			`Event 0x${BigInt(id).toString(16)} expects intents [ ${x
+				.toArray()
+				.join(", ")} ] but bot has [ ${bf.toArray().join(", ")} ]`
+		);
+		this.client = client;
+		this.actual = bf;
+		this.expect = x;
+		this.name = "IntentsError";
+	}
 }
